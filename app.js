72f28004bd2267e86631208e6d7a58d5410b84ac
Theunis <tduminy6@gmail.com>
2021-08-18 13:17:14 +0200

    change PORT

--- app.js
+++ app.js
@@ -113,7 +113,7 @@ app.get('/', (req, res) => {
   // res.send('hello world');
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}...`);
 });
