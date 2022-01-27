d3f1a925ef4f3fca36d06f0ff5773ddb6bcd5496
johan <smal.jg@gmail.com>
2022-01-27 09:16:13 +0200

    change h2h points when a tie occurs

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ function createPlayersObject(players) {
 function sortPlayersObject(standingsData) {
   for (const rank of standingsData) {
     const thePlayer = playerDetails.find((p) => p.id === rank.league_entry);
-    thePlayer.head_to_head_rank = standingsData.filter((s) => s.total === rank.total).sort((a, b) => a.rank - b.rank)[0].rank_sort;
+    thePlayer.head_to_head_rank = standingsData.filter((s) => s.total === rank.total).sort((a, b) => b.rank - a.rank)[0].rank_sort;
     thePlayer.head_to_head_points = rank.total
     thePlayer.total_points = rank.points_for;
     thePlayer.head_to_head_total = rank.total;
