bca0c28b6d097c744e0250694626b311eca9dc0f
Theunis <tduminy6@gmail.com>
2022-01-27 09:39:52 +0200

    new color

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ function sortPlayersObject(standingsData) {
     thePlayer.head_to_head_rank = standingsData
       .filter((s) => s.total === rank.total)
       .sort((a, b) => b.rank - a.rank)[0].rank_sort;
-    thePlayer.head_to_head_points = rank.total
+    thePlayer.head_to_head_points = rank.total;
     thePlayer.total_points = rank.points_for;
     thePlayer.points_against = rank.points_against;
     thePlayer.head_to_head_total = rank.total;
