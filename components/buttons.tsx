01ede8f840b655757816d3f44936bdf86d9840b1
Theunis <tduminy6@gmail.com>
2023-08-22 18:30:45 +0200

    detailed view, back link, other shit

--- components/buttons.tsx
+++ components/buttons.tsx
@@ -1,18 +1,18 @@
 import styles from '@/../styles/Button.module.css';
-import { useRouter } from 'next/router';
-import Link from 'next/link';
 
 interface HeaderSectionProps {
   showTitle?: boolean;
 }
 
 export default function Buttons(props: HeaderSectionProps) {
-  // const router = useRouter();
-  // const path = router.pathname;
-
   return (
-    <div className={styles.homeNav}>
-      <p>hello</p>
+    <div className={styles.buttonLayout}>
+      <a className={styles.buttonLeft} href='/detail'>
+        Detail view
+      </a>
+      <a className={styles.buttonRight} href='https://draft.premierleague.com/team/my'>
+        Draft team
+      </a>
     </div>
   );
 }
--- components/header.tsx
+++ components/header.tsx
@@ -4,25 +4,26 @@ import Link from 'next/link';
 
 interface HeaderSectionProps {
   showTitle?: boolean;
+  title: string;
 }
 
 export default function Header(props: HeaderSectionProps) {
-  const { showTitle = true } = props;
+  const { showTitle = true, title } = props;
   const router = useRouter();
   const path = router.pathname;
 
   return (
     <div className={styles.homeNav}>
-      {path !== '/' && (
-        <Link href={'/'} className={styles.backButton}>
-          <i className='fa fa-arrow-left'></i> Back to Home
-        </Link>
-      )}
-      <br></br>
-      <div className={styles.description}>
+      <div>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img className={styles.banner} src='../premier-league-banner-teal.png' alt='premier league logo' />
-        {showTitle && <h1>Draft League Standings</h1>}
+        {showTitle && <h1>{title}</h1>}
+
+        {path !== '/' && (
+          <Link href={'/'} className={styles.backButton}>
+            <i className='fa fa-arrow-left'></i> Back to Home
+          </Link>
+        )}
       </div>
     </div>
   );
