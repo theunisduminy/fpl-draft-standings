dbd0772bd29a0931a2a45a9be6cbb677b271f910
Theunis <tduminy6@gmail.com>
2024-08-04 15:38:16 +0200

    update all tables for new standings routing

--- app/detail/page.tsx
+++ app/detail/page.tsx
@@ -3,19 +3,20 @@ import { useState } from 'react';
 import DraftCurrentFixtures from './components/DraftCurrentFixtures';
 import ViewButton from '@/components/ViewButtons';
 import DraftResults from '@/app/detail/components/DraftResults';
+import DraftFixtures from '@/app/detail/components/DraftFixtures';
 
 export default function DetailView() {
-  const [activeTableMatches, setActiveTableMatches] = useState('fixtures');
+  const [activeTableMatches, setActiveTableMatches] = useState('currentFixtures');
 
   return (
     <main className={`justify-start py-10 md:py-20`}>
       <h1 className='text-[#310639] text-4xl pb-5 font-semibold animate-fade-up text-center'>
         Matches
       </h1>
-      <div className='md:grid-cols-3 grid grid-cols-2 gap-4 pb-10'>
+      <div className='grid grid-cols-2 gap-4 pb-10'>
         <ViewButton
-          isActive={activeTableMatches === 'fixtures'}
-          onClick={() => setActiveTableMatches('fixtures')}
+          isActive={activeTableMatches === 'currentFixtures'}
+          onClick={() => setActiveTableMatches('currentFixtures')}
         >
           GW Live
         </ViewButton>
@@ -25,9 +26,18 @@ export default function DetailView() {
         >
           Results
         </ViewButton>
+        <ViewButton
+          isActive={activeTableMatches === 'fixtures'}
+          onClick={() => setActiveTableMatches('fixtures')}
+        >
+          Fixtures
+        </ViewButton>
       </div>
-      <div className='mt-4'>{activeTableMatches === 'fixtures' && <DraftCurrentFixtures />}</div>
+      <div className='mt-4'>
+        {activeTableMatches === 'currentFixtures' && <DraftCurrentFixtures />}
+      </div>
       <div className='mt-4'>{activeTableMatches === 'results' && <DraftResults />}</div>
+      <div className='mt-4'>{activeTableMatches === 'fixtures' && <DraftFixtures />}</div>
     </main>
   );
 }
