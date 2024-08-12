db89589a2e1ef6a5a9afe621a30d523e6532bc64
Theunis <tduminy6@gmail.com>
2024-08-12 17:42:56 +0200

    Test no data

--- app/api/matches/route.ts
+++ app/api/matches/route.ts
@@ -1,16 +1,11 @@
 import { NextResponse } from 'next/server';
 import { Match } from '@/interfaces/match';
-import standing from '@/data/league-details.json';
+// import standing from '@/data/league-details.json';
 
 async function fetchData(): Promise<{ matches: Match[] }> {
   try {
-    if (process.env.NODE_ENV === 'development') {
-      const res = standing;
-      return await res;
-    } else {
-      const res = await fetch('https://draft.premierleague.com/api/league/5525/details');
-      return await res.json();
-    }
+    const res = await fetch('https://draft.premierleague.com/api/league/5525/details');
+    return await res.json();
   } catch (err) {
     console.error(err);
     throw err;
--- app/api/standings/route.ts
+++ app/api/standings/route.ts
@@ -7,6 +7,10 @@ import { sortPlayersObject } from './utils/sortPlayersObject';
 
 export const GET = async (req: Request, res: Response) => {
   try {
+    const data = await fetchData();
+
+    console.log(data);
+
     const { matches, league_entries, standings } = await fetchData();
 
     // Create player objects
